4f3acc70ff30f8088bff06a0e9feaf95bdf360e1
cora2406 <audrey.corbeil.therrien@usherbrooke.ca>
2016-08-24 16:22:53 -0400

    Added root importer for post array event data. Adding and modifying EnergyDiscrimination code for NSS-MIC2016

---
+++ EnergyDiscrimination.py
@@ -0,0 +1,105 @@
+# ! /usr/bin/env python
+# coding=utf-8
+__author__ = 'acorbeil'
+
+## Utilities
+from CCoincidenceCollection import CCoincidenceCollection
+import CEnergyDiscrimination
+from CTdc import CTdc
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+## Importers
+from Importer.ImporterROOT import ImporterRoot
+
+## Algorithms
+from TimingAlgorithms.CAlgorithmBlueExpectationMaximisation import CAlgorithmBlueExpectationMaximisation
+from TimingAlgorithms.CAlgorithmSinglePhoton import CAlgorithmSinglePhoton
+
+# Discriminators
+from DarkCountDiscriminator import DiscriminatorWindowDensity
+from DarkCountDiscriminator import DiscriminatorDualWindow
+from DarkCountDiscriminator import DiscriminatorMultiWindow
+
+def run_timing_algorithm(algorithm, event_collection):
+
+    # Evaluate the resolution of the collection
+    results = algorithm.evaluate_collection_timestamps(event_collection)
+
+    # Print the report
+    results.print_results()
+    return results.fetch_fwhm_time_resolution()
+
+def collection_procedure(filename):
+    # File import -----------------------------------------------------------
+    importer = ImporterRoot()
+    importer.open_root_file(filename)
+    event_collection = importer.import_all_spad_events(100)
+    print("#### Opening file ####")
+    print(filename)
+    # Energy discrimination -------------------------------------------------
+    CEnergyDiscrimination.discriminate_by_energy(event_collection, low_threshold_kev=425,
+                                                 high_threshold_kev=700)
+
+    # Filtering of unwanted photon types ------------------------------------
+    event_collection.remove_unwanted_photon_types(remove_thermal_noise=False, remove_after_pulsing=False,
+                                                  remove_crosstalk=False, remove_masked_photons=True)
+
+    event_collection.save_for_hardware_simulator()
+
+    # Sharing of TDCs --------------------------------------------------------
+    # event_collection.apply_tdc_sharing(pixels_per_tdc_x=1, pixels_per_tdc_y=1)
+
+    # First photon discriminator ---------------------------------------------
+    # DiscriminatorMultiWindow.DiscriminatorMultiWindow(event_collection)
+    DiscriminatorDualWindow.DiscriminatorDualWindow(event_collection)
+
+    # Making of coincidences -------------------------------------------------
+    coincidence_collection = CCoincidenceCollection(event_collection)
+
+    # Apply TDC - Must be applied after making the coincidences because the
+    # coincidence adds a random time offset to pairs of events
+    tdc = CTdc(system_clock_period_ps=5000, tdc_bin_width_ps=10, tdc_jitter_std=15)
+    tdc.get_sampled_timestamps(coincidence_collection.detector1)
+    tdc.get_sampled_timestamps(coincidence_collection.detector2)
+
+    return event_collection, coincidence_collection
+
+def main_loop():
+
+    matplotlib.rc('xtick', labelsize=18)
+    matplotlib.rc('ytick', labelsize=18)
+    matplotlib.rc('legend', fontsize=16)
+    font = {'family': 'normal',
+            'size': 18}
+
+    matplotlib.rc('font', **font)
+    arraysize = 1000
+
+    filename = "/home/cora2406/FirstPhotonEnergy/spad_events/LYSO1110TW_Baseline.root"
+
+    event_collection, coincidence_collection = collection_procedure(filename)
+
+    energy_resolution = event_collection.get_energy_resolution()
+    max_single_photon = 8
+    max_BLUE = 10
+
+    if(max_single_photon > event_collection.qty_of_photons):
+        max_single_photon = event_collection.qty_of_photons
+
+    print "\n### Calculating time resolution for different algorithms ###"
+
+    # Running timing algorithms ------------------------------------------------
+    for p in range(1, max_single_photon):
+        algorithm = CAlgorithmSinglePhoton(photon_count=p)
+        crystal_tr_sp_fwhm[j, i, p-1] = run_timing_algorithm(algorithm, coincidence_collection)
+
+    if(max_BLUE > event_collection.qty_of_photons):
+        max_BLUE = event_collection.qty_of_photons
+
+    for p in range(2, max_BLUE):
+        algorithm = CAlgorithmBlueExpectationMaximisation(coincidence_collection, photon_count=p)
+        crystal_tr_BLUE_fwhm[j, i, p-2] = run_timing_algorithm(algorithm, coincidence_collection)
+
+main_loop()
---
+++ Importer/ImporterROOT.py
@@ -0,0 +1,59 @@
+# ! /usr/bin/env python
+# coding=utf-8
+__author__ = 'acorbeil'
+
+from ROOT import TFile
+from Importer import ImporterUtilities
+from CEventCollection import CEventCollection
+import numpy as np
+
+
+class ImporterRoot:
+
+    file = None
+    tree = None
+
+    def __init__(self):
+        pass
+
+    def open_root_file(self, filename):
+        self.file = TFile(filename)
+        self.tree = self.file.Get('tree')
+
+    def import_all_spad_events(self, number_of_events, start=0):
+        # This loads the entry in tree
+        if number_of_events == 0:
+            number_of_events = self.mytree.GetEntries()-start
+
+        #Initialize empty arrays
+        max_elements = 128
+        global_time = np.zeros((number_of_events, max_elements))
+        pixel_x_coord = np.zeros((number_of_events, max_elements))
+        pixel_y_coord = np.zeros((number_of_events, max_elements))
+        trigger_type = np.zeros((number_of_events, max_elements))
+        event_ID = np.zeros(number_of_events)
+        photon_count = np.zeros(number_of_events)
+        spad_trigger_count = np.zeros(number_of_events)
+        avalanche_count = np.zeros(number_of_events)
+
+        valid_event_count=0;
+        for event_id in range(start, number_of_events+start):
+            self.tree.GetEntry(event_id)
+            test_global_time = np.array(self.tree.GlobalTime[:])
+            if np.size(test_global_time) > 100000 or np.size(test_global_time)< max_elements:
+                continue
+
+            global_time[valid_event_count, :] = test_global_time[0:max_elements]
+            pixel_x_coord[valid_event_count, :] = np.array(self.tree.SpadX[0:max_elements])
+            pixel_y_coord[valid_event_count, :] = np.array(self.tree.SpadY[0:max_elements])
+            trigger_type[valid_event_count, :] = np.array(self.tree.TriggerType[0:max_elements])
+            event_ID[valid_event_count] = self.tree.Event
+            photon_count[valid_event_count] = self.tree.PhotonCount
+            spad_trigger_count[valid_event_count] = self.tree.SpadTriggeredCount
+            avalanche_count[valid_event_count] = self.tree.AvalancheCount
+            valid_event_count = + 1
+
+        return CEventCollection(event_ID, global_time, spad_trigger_count, trigger_type, pixel_x_coord, pixel_y_coord)
+
+    def close_file(self):
+        self.file.Close()
